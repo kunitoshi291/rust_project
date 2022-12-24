bb2e503e0d80ece538df54029f98cf662932709a
Takekuni <takevadogu@outlook.jp>
2022-12-24 17:16:30 +0900

    Add hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-05e756cb8f9be248.d
@@ -0,0 +1,5 @@
+/Users/takeuchikuniju/rust_projects/hello_cargo/target/debug/deps/hello_cargo-05e756cb8f9be248.rmeta: src/main.rs
+
+/Users/takeuchikuniju/rust_projects/hello_cargo/target/debug/deps/hello_cargo-05e756cb8f9be248.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/takeuchikuniju/rust_projects/hello_cargo/target/debug/hello_cargo: /Users/takeuchikuniju/rust_projects/hello_cargo/src/main.rs
